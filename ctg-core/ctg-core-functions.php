3a3c18cd3cf16c22b79e9a4be447ea6497b02ea4
dustindelgross <dustin.delgross@icloud.com>
2023-02-09 19:10:38 -0500

    Remove celebratorcreator dquote> Remove some empty files dquote> Remove some obsolete functions dquote> Remove some useless commented code dquote> Add some common admin styles dquote> Change the admin nav menu structure Add doc strings to ctg-core-functions.php

--- ctg-core/ctg-core-functions.php
+++ ctg-core/ctg-core-functions.php
@@ -1,10 +1,34 @@
-<?php 
+<?php
+/**
+ * CTG Core Functions
+ * A collection of utility functions for CTG Core.
+ *
+ * @package CTG_Core
+ * @subpackage CTG_Core\Functions
+ * @since 0.1.0
+ */
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * CTG Core Is Valid Email
+ *
+ * A utility function to validate an email address.
+ *
+ * @param string $text The email address to validate.
+ * @return bool True if valid, false if not.
+ */
 function ctg_core_is_valid_email( $text = '' ) {
 	return preg_match( '/\A[\w]{2,20}\.?[\w?]{2,20}@[\a-zA-Z0-9]{2,20}\.[a-z]{2,10}/', $text );
 }
 
+/**
+ * CTG Core Is CTG Email
+ *
+ * A utility function to validate a CTG email address.
+ *
+ * @param string $text The email address to validate.
+ * @return bool True if valid, false if not.
+ */
 function ctg_core_is_ctg_email( $text = '' ) {
 	return preg_match( '/\A[\w]{2,20}\.?[\w?]{2,20}@celebrationtitlegroup\.com/', $text );
 }
@@ -12,7 +36,7 @@ function ctg_core_is_ctg_email( $text = '' ) {
 /**
  * CTG Core Validate Address
  * A utility function to validate an address.
- * 
+ *
  * @param mixed $args Array of arguments to use. {
  * 		@var string $street_primary Primary street number.
  * 		@var string $street_secondary Secondary street number.
@@ -89,6 +113,15 @@ function ctg_core_validate_address($args = array())
 	return $feedback;
 
 }
+
+/**
+ * CTG Core Get Parent Category By Slug
+ *
+ * A utility function to get a parent category by slug.
+ *
+ * @param string $slug The slug of the category to get.
+ * @return object|bool The category object or false if not found.
+ */
 function ctg_core_get_parent_category_by_slug( $slug = '' ) {
 	if ( empty($slug)) {
 		return;
@@ -96,6 +129,14 @@ function ctg_core_get_parent_category_by_slug( $slug = '' ) {
 	return get_term_by( 'slug', $slug, 'category' );
 }
 
+/**
+ * CTG Core Is Component Category
+ *
+ * A utility function to check if a post is in a category or a subcategory.
+ *
+ * @param string $slug The slug of the category to check.
+ * @return bool True if in category, false if not.
+ */
 function ctg_core_is_component_category( $slug = '' ) {
 
 	$is_category_component = false;
@@ -126,6 +167,12 @@ function ctg_core_is_component_category( $slug = '' ) {
 
 }
 
+/**
+ * CTG Core Get Parent Category By Slug
+ * A utility function to get a parent category by slug.
+ * @param string $slug The slug of the category to get.
+ * @return object|bool The category object or false if not found.
+ */
 function ctg_core_check_post_subcategories( $categories, $_post = null ) {
 
 	foreach ( (array) $categories as $category ) {
@@ -136,48 +183,103 @@ function ctg_core_check_post_subcategories( $categories, $_post = null ) {
 	return false;
 }
 
+/**
+ * CTG Core Get Parent Category By Slug
+ * A utility function to get a parent category by slug.
+ * @param string $slug The slug of the category to get.
+ * @return object|bool The category object or false if not found.
+ */
 function ctg_get_version() {
 	return ctg()->version;
 }
 
+/**
+ * CTG Get Option
+ * A utility function to get a CTG option.
+ *
+ * @param string $option_name The name of the option to get.
+ * @param string $default The default value to return if the option is not set.
+ * @return string The option value.
+ */
 function ctg_get_option( $option_name, $default = '' ) {
 	$value = get_option( $option_name, $default );
 	return apply_filters( 'ctg_get_option', $value );
 }
 
+/**
+ * CTG Update Option
+ * A utility function to update a CTG option.
+ *
+ * @param string $option_name The name of the option to update.
+ * @param string $value The value to set the option to.
+ * @return bool True if the option was updated, false if not.
+ */
 function ctg_update_option( $option_name, $value ) {
 	return update_blog_option( get_current_blog_id(), $option_name, $value );
 }
 
+/**
+ * CTG Delete Option
+ * A utility function to delete a CTG option.
+ *
+ * @param string $option_name The name of the option to delete.
+ * @return bool True if the option was deleted, false if not.
+ */
+function ctg_delete_option( $option_name ) {
+	return delete_blog_option( get_current_blog_id(), $option_name );
+}
+
+/**
+ * CTG Get Admin URL
+ * A utility function to get the admin URL.
+ *
+ * @param string $path The path to append to the admin URL.
+ * @param string $scheme The scheme to use. Default is 'admin', which obeys force_ssl_admin() and is_ssl().
+ * @return string The admin URL link with optional path appended.
+ */
+function ctg_get_admin_url( $path = '', $scheme = 'admin' ) {
+	$url = admin_url( $path, $scheme );
+	return $url;
+}
+
+/**
+ * CTG Is Post Request
+ * A utility function to check if the current request is a POST request.
+ *
+ * @return bool True if the current request is a POST request, false if not.
+ */
 function ctg_is_post_request() {
 	return (bool) ( 'POST' === strtoupper( $_SERVER['REQUEST_METHOD'] ) );
 }
 
+/**
+ * CTG Is Get Request
+ * A utility function to check if the current request is a GET request.
+ *
+ * @return bool True if the current request is a GET request, false if not.
+ */
 function ctg_is_get_request() {
 	return (bool) ( 'GET' === strtoupper( $_SERVER['REQUEST_METHOD'] ) );
 }
 
-function ctg_get_admin_url( $path = '', $scheme = 'admin' ) {
-
-	$url = admin_url( $path, $scheme );
-
-	return $url;
+/**
+ * CTG Is Ajax Request
+ * A utility function to check if the current request is an AJAX request.
+ *
+ * @return bool True if the current request is an AJAX request, false if not.
+ */
+function ctg_is_ajax_request() {
+	return (bool) ( defined( 'DOING_AJAX' ) && DOING_AJAX );
 }
+
 
 function ctg_core_get_components( $type = 'all' ) {
 
 	$required_components = array(
-		'celebratorcreator' => array(
-			'title'       => __( 'Celebrator Creator', 'ctg' ),
-			'settings'    => ctg_get_admin_url( add_query_arg( array( 
-				'page' => 'ctg-settings', 
-				'tab' => 'ctg-celebratorcreator' ) , 'admin.php' ) ),
-			'description' => __( 'Allow site admins to create, modify, and delete CTG celebrators, confetti cannons, and more.', 'ctg' ),
-		),
 		'locations' => array(
 			'title'       => __( 'Locations', 'ctg' ),
-			'settings'    => ctg_get_admin_url( add_query_arg( array( 
-				'page' => 'ctg-settings', 
+			'settings'    => ctg_get_admin_url( add_query_arg( array(
+				'page' => 'ctg-settings',
 				'tab' => 'ctg-locations' ) , 'admin.php' ) ),
 			'description' => __( 'Allow site admins to create, modify, and delete CTG locations.', 'ctg' ),
 		),
@@ -213,6 +315,7 @@ function ctg_core_get_components( $type = 'all' ) {
 
 }
 
+
 function ctg_displayed_user_id() {
 	$ctg = ctg();
 	$id = !empty( $ctg->displayed_user->id )
@@ -222,19 +325,24 @@ function ctg_displayed_user_id() {
 	return (int) apply_filters( 'ctg_displayed_user_id', $id );
 }
 
-function ctg_core_get_directory_page_default_titles() {
-	$page_default_titles = array(
-		'celebratorcreator'	=> __( 'Celebrators', 'ctg' ),
-	);
 
-	return apply_filters( 'ctg_core_get_directory_page_default_titles', $page_default_titles );
-}
-
+/**
+ * CTG Core Get Table Prefix
+ * A utility function to get the table prefix.
+ *
+ * @return string The table prefix.
+ */
 function ctg_core_get_table_prefix() {
 	global $wpdb;
 	return apply_filters( 'ctg_core_get_table_prefix', $wpdb->base_prefix );
 }
 
+/**
+ * CTG Core Get Site Path
+ * A utility function to get the site path.
+ *
+ * @return string The site path.
+ */
 function ctg_core_get_site_path() {
 
 	$site_path = (array) explode( '/', home_url() );
@@ -249,7 +357,7 @@ function ctg_core_get_site_path() {
 
 		if ( !count( $site_path ) ) {
 			$site_path = '/';
-		} else { 
+		} else {
 			$site_path = '/' . implode( '/', $site_path ) . '/';
 		}
 	}
@@ -257,6 +365,14 @@ function ctg_core_get_site_path() {
 	return apply_filters( 'ctg_core_get_site_path', $site_path );
 }
 
+/**
+ * CTG Parse Args
+ * A utility function to parse arguments.
+ *
+ * @param string|array|object $args The value being parsed.
+ * @param array $defaults The default values.
+ * @return array The parsed arguments.
+ */
 function ctg_parse_args( $args, $defaults = array() ) {
 
 	if ( is_object( $args ) ) {
@@ -274,34 +390,48 @@ function ctg_parse_args( $args, $defaults = array() ) {
 	return $r;
 }
 
-
-
+/**
+ * CTG Core Redirect
+ * A utility function to redirect to a specific page.
+ *
+ * @param string $slug The slug of the page to redirect to.
+ * @param int $status The HTTP status code to use.
+ * @return void
+ */
 function ctg_core_redirect( $slug = '', $status = 302 ) {
 
 	wp_safe_redirect( home_url() . "/{$slug}", $status );
 
 }
 
+/**
+ * CTG Core Add Message
+ * A utility function to add a message to the cookie.
+ *
+ * @param string $message The message to add.
+ * @param string $type The type of message to add.
+ * @return void
+ */
 function ctg_core_add_message( $message, $type = '' ) {
 
 	if ( empty( $type ) ) {
 		$type = 'success';
 	}
 
-	@setcookie( 
-		'ctg-message', 
-		$message, 
-		time() + 60 * 60 * 24, 
-		COOKIEPATH, 
-		COOKIE_DOMAIN, 
-		is_ssl() 
+	@setcookie(
+		'ctg-message',
+		$message,
+		time() + 60 * 60 * 24,
+		COOKIEPATH,
+		COOKIE_DOMAIN,
+		is_ssl()
 	);
-	@setcookie( 'ctg-message-type', 
-			   $type, 
-			   time() + 60 * 60 * 24, 
-			   COOKIEPATH, 
-			   COOKIE_DOMAIN, 
-			   is_ssl() 
+	@setcookie( 'ctg-message-type',
+			   $type,
+			   time() + 60 * 60 * 24,
+			   COOKIEPATH,
+			   COOKIE_DOMAIN,
+			   is_ssl()
 			  );
 
 	$ctg = ctg();
@@ -310,6 +440,14 @@ function ctg_core_add_message( $message, $type = '' ) {
 
 }
 
+/**
+ * CTG Core Setup Message
+ * A utility function to setup the message.
+ *
+ * @global object $ctg The CTG object.
+ *
+ * @return void
+ */
 function ctg_core_setup_message() {
 
 	$ctg = ctg();
@@ -334,6 +472,12 @@ function ctg_core_setup_message() {
 }
 add_action( 'ctg_actions', 'ctg_core_setup_message', 3 );
 
+/**
+ * CTG Core Render Message
+ * A utility function to render the message.
+ *
+ * @return void
+ */
 function ctg_core_render_message() {
 
 	$ctg = ctg();
@@ -352,8 +496,13 @@ function ctg_core_render_message() {
 	}
 }
 
-
-
+/**
+ * CTG SMTP Init
+ * A utility function to initialize SMTP.
+ *
+ * @param object $phpmailer The PHPMailer object.
+ * @return void
+ */
 function ctg_smtp_init( $phpmailer ) {
 	$phpmailer->Host		= SMTP_HOST;
 	$phpmailer->Port		= SMTP_PORT;
@@ -367,14 +516,28 @@ function ctg_smtp_init( $phpmailer ) {
 	$phpmailer->IsSMTP();
 
 }
+add_action( 'phpmailer_init', 'ctg_smtp_init' );
 
+/**
+ * CTG Sender Email
+ * A utility function to set the sender email.
+ *
+ * @param string $original_email_address The original email address.
+ * @return string The new email address.
+ */
 function ctg_sender_email( $original_email_address ) {
 	return 'dustin@aws-ses-smtp.celebrationtitlegroup.com';
 }
+add_filter( 'wp_mail_from', 'ctg_sender_email' );
+
+/**
+ * CTG Sender Name
+ * A utility function to set the sender name.
+ *
+ * @param string $original_email_from The original email from.
+ * @return string The new email from.
+ */
 function ctg_sender_name( $original_email_from ) {
 	return 'Celebration Title Group';
 }
-
-add_action( 'phpmailer_init', 'ctg_smtp_init' );
-add_filter( 'wp_mail_from', 'ctg_sender_email' );
 add_filter( 'wp_mail_from_name', 'ctg_sender_name' );
